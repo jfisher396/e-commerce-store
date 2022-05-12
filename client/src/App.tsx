8918487587a789f3b75bea6e26e11a69f5918ec1
James Fisher <jfisher396@hotmail.com>
2022-05-12 13:35:40 -0500

    added setState function for products and useEffect to call API

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([
+    {
+      name: "product1",
+      price: 200.0,
+    },
+    {
+      name: "product2",
+      price: 100.0,
+    },
+  ]);
+
+  useEffect(() => {
+    fetch('http://localhost:5001/api/products') 
+    .then((response) => response.json())
+    .then((data) => setProducts(data))
+  }, []) 
+
+  function addProduct() {
+    setProducts([
+      ...products,
+      {
+        name: "product",
+        price: 300.0,
+      },
+    ]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Re-Store</h1>
+      <ul>
+        {products.map((item) => (
+          <li key={item.name}>
+            {item.name} - {item.price}
+          </li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>Add Product</button>
     </div>
   );
 }
