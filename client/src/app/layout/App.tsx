bb3478fd23962e61f5668a3e39181927f248d983
James Fisher <jfisher396@hotmail.com>
2022-05-14 14:20:00 -0500

    created additional pages and routing

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -7,6 +7,11 @@ import {
 } from "@mui/material";
 import Header from "./Header";
 import { useState } from "react";
+import { Route } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import AboutPage from "../../features/about/AboutPage";
+import ContactPage from "../../features/contact/ContactPage";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -29,6 +34,11 @@ function App() {
       <CssBaseline />
       <Header paletteModeHandler={paletteModeHandler}></Header>
       <Container>
+        <Route path='/' component={HomePage} />
+        <Route path='/catalog' component={Catalog} />
+        <Route path='/catalog/:id' component={ProductDetails} />
+        <Route path='/about' component={AboutPage} />
+        <Route path='/contact' component={ContactPage} />
         <Catalog />
       </Container>
     </ThemeProvider>
---
+++ client/src/features/catalog/ProductDetails.tsx
@@ -0,0 +1,10 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+export default function ProductDetails() {
+  return (
+    <Typography variant='h2'>
+        Product details
+    </Typography>
+  )
+}
