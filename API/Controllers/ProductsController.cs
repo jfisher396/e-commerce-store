6a2a1eea0a634bdf0b007cf0294872ba541ef35c
James Fisher <jfisher396@hotmail.com>
2022-05-27 10:00:30 -0500

    reverted back to before adding BaseApiController

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -10,15 +10,15 @@ using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
 
 namespace API.Controllers
 {
-
-    public class ProductsController : BaseApiController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
     {
         private readonly StoreContext _context;
-        
+
         public ProductsController(StoreContext context)
         {
             _context = context;
-           
 
         }
 
